0946402ed4294d60818c2e71857e122b477da64a
omerfcoskun <36162449+omerfcoskun@users.noreply.github.com>
2020-11-07 11:53:13 +0300

    Update Recursive.py

--- Code/Recursive.py
+++ Code/Recursive.py
@@ -1,17 +1,15 @@
-
 print("N :")
 N=int(input())
 
 def Fibonacci(pos):
-    if pos <= 1:
+    if pos == 1:
         return 0
     if pos == 2:
         return 1
-    fib1 = Fibonacci(pos - 1)
-    fib2 = Fibonacci(pos - 2)
-    fibn = fib1 + fib2
-    return fibn
+
+    return Fibonacci(pos-1)+Fibonacci(pos-2)
 
 for i in range(1,N):
     wanted = Fibonacci(i)
     print(wanted)
+
